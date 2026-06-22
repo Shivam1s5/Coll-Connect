244abe74eb3cf2435dd00ed2712eda6f1fc650fb
Shivam1s5 <Shivamtyagiji15@gmail.com>
2026-06-22 10:26:29 +0530

    Updates42

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -4,11 +4,16 @@ const ADMIN_EMAIL = process.env.ADMIN_EMAIL || 'coder.st.15@gmail.com';
 const ADMIN_APP_PASSWORD = process.env.ADMIN_APP_PASSWORD || 'nimscdzabzpvzvki';
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // Use implicit TLS
   auth: {
     user: ADMIN_EMAIL,
     pass: ADMIN_APP_PASSWORD
-  }
+  },
+  connectionTimeout: 5000, // 5 seconds
+  greetingTimeout: 5000,
+  socketTimeout: 5000
 });
 
 const sendEmail = async (to, subject, html) => {
